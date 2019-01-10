87c34e1319c8705ff4a069ac0346892af7f01d38
Damjan Stevanoski <dste3631@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:17:48 +1100

    added second case

--- demobot.py
+++ demobot.py
@@ -33,7 +33,7 @@ def weather():
 @app.route('/lockout', methods = ['GET', 'POST'])
 def lockout():
     location = request.values.get('text')
-    if location == None:
+    if location == None or location == " ":
         return "Where are you locked out?"
     else:
         return f'Sending Smerity to {location} to save you'
