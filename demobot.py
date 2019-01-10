d65d2c866880e30d8b23cf74b120f685d4e16780
Damjan Stevanoski <dste3631@shared.sydney.edu.au>
2019-01-10 11:05:02 +1100

    first commit

---
+++ demobot.py
@@ -0,0 +1,32 @@
+# Import flask
+from flask import Flask, request
+
+# Create your app (web server)
+app = Flask(__name__)
+
+
+# When people visit the home page '/' use the hello_world function
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# You can access demobot’s greet command via <your website>/greet
+@app.route('/greet')
+def greet_person():
+    # Get the value of the 'name' query parameter
+    # request.values is a dictionary (cool!)
+    name = request.values.get('name')
+    # This bot says hi to every name it gets sent!
+    return f'hi {name}!'
+
+@app.route('/weather')
+def weather():
+    temp = request.values.get('temp')
+
+    if int(temp) > 30:
+        return "It's too hot!"
+    else:
+        return f'It is {temp} degrees today'
+if __name__ == '__main__':
+    # Start the web server!
+    app.run()
